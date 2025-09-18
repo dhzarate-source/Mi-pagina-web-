df089efa8373857475b425d01820dad5d67d65e4
dhzarate-source <dh.zarate@ebc.edu.mx>
2025-09-18 07:48:44 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// =========================
+// JavaScript 
+// =========================
+
+// Solo un ejemplo para comprobar que funciona
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("JS cargado correctamente - App de moda");
+  });
